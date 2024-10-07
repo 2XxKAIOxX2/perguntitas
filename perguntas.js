56ca767e82aa63aa47516503b0f054a76a9e05d8
2XxKAIOxX2 <142432548+2XxKAIOxX2@users.noreply.github.com>
2024-10-07 12:11:07 -0300

    tryj

---
+++ perguntas.js
@@ -0,0 +1,145 @@
+criaCartao(
+    'Matematica',
+    ' 1) Calcule o termo de delta ? ultilizando a formula de bhaskara',
+    'Ultiize a formula  ? = b² – 4ac')
+    
+    
+    
+    
+    criaCartao(
+    'Historia',
+    ' 2) Qual foi o estopim da primeira Guerra Mundial',
+    'Foi o assassinato do arquiduque Francisco Ferdinando')
+    
+    
+    
+    criaCartao(
+    'Quimica',
+    ' 3) Oque abrange a quimica organica?',
+    ' Abrange as ligações de carbono')
+    
+    
+    
+    
+    criaCartao(
+    'Biologia',
+    ' 4) Oque são genes?',
+    ' É o cojunto e celulas de DNA que determinam as suas caracteristicas')
+    
+    
+    
+    
+    criaCartao(
+    'Português',
+    ' 5) Cite ao menos uma caracteristica do Naturalismo ',
+    ' A animalização do homem,Radicalização do realismo',
+    ' 5)Oposição aos ideais românticos,Cientificismo e Determinismo,Positivismo e Darwinismo, Romance experimental,Temas sociais, obscuros e polêmicos,Personagens patológicas (mórbidas, desequilibradas e doentias),Foco na análise de comportamentos humanos,Sensualismo e erotismo')
+    
+    
+    
+    criaCartao(
+    'Geografia',
+    ' 6)Qual ano aproximadamente ocorreu a primeira Revolução industrial?',
+    'Mais ou menos em 1750')
+    
+    
+    
+    criaCartao(
+    'Matematica',
+    ' 7)Qual a formula para calcular a área de um trapezio?',
+    'Áreatrapézio=(B+b)·h/2')
+
+    
+    
+    criaCartao(
+    'Quimica',
+    ' 8) Oque é um anel benzenico?',
+    ' É o composto que forma os hidrocarbonetos aromáticos')
+    
+    
+    
+    criaCartao(
+    'Historia',
+    ' 9) Em que ano ocorreu a Revolução francesa?',
+    ' Ocorreu em 1789')
+    
+    
+    
+    criaCartao(
+    'Biologia',
+    ' 10) Oque são genes alelos?',
+    'São caracterizados por estarem localizados no mesmo lugar nos cromossomos homólogos, o lócus')
+    
+    
+    
+    criaCartao(
+    'Português',
+    ' 11) Quando foi criado o naturalismo?',
+     ' Foi criado na segunda metade do seculo 19')
+    
+    
+    
+    criaCartao(
+    'Geografia',
+    ' 12) Qual a maior bacia hidrografica do mundo?',
+    ' E a bacia amazônca')
+    
+    
+    
+    criaCartao(
+    'Matematica',
+    ' 13) Quanto graus tem um triangulo?',
+    '180 graus')
+    
+    
+    
+    criaCartao(
+    'Historia',
+    ' 14) Em que ano começou a Ditadura militar?',
+    'começou em 1964')
+    
+    
+    
+    criaCartao(
+    'Biologia',
+    ' 15) Oque são celulas procariontes?',
+    ' São celulas que não contem núcleo')
+    
+    
+    
+    criaCartao(
+    'Quimica',
+    ' 16) Oque é eletroquimica?',
+    ' É a area que estuda reações de oxirredução ou redox')
+    
+    
+    
+    criaCartao(
+    'Geografia',
+    ' 17) Onde começou a primeiora revolução industrial',
+    ' Começou na Inglaterra')
+    
+    
+    
+    criaCartao(
+    'Matematica',
+    ' 18) Para que serve o teorema de pitagoras?',
+    ' Para descobrir a medida de um dos lados de um triângulo jà sabendo as duas outras medidas')
+    
+    
+    
+    
+    criaCartao(
+    'Historia',
+    ' 19) Quando Getulio Vargas se suicidou?',
+    'Foi no dia 24 de agosto de 1954')
+    
+    
+    
+    criaCartao(
+    'Geografia',
+    ' 20) Quem descobriu o Brasil?',
+    'Os indio, brincadeira foi Pedro Álvares Cabral ')
+    
+    
+    
