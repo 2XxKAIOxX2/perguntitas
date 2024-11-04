aa2dabae05efe80f0cc6587abfc9b7f904f8d88c
2XxKAIOxX2 <142432548+2XxKAIOxX2@users.noreply.github.com>
2024-11-04 09:30:30 -0300

    g

--- 2.js
+++ 2.js
@@ -16,7 +16,7 @@ function criaCartao(categoria, pergunta, resposta){
     container.appendChild(cartao)
 }
 function viraCartao(card){
-const resposta=card.querySelection('.respotas-cartao');
+const resposta=card.querySelector('.respotas-cartao');
 if(resposta.style.display==='none'){
 resposta.style.display='block';
 }
